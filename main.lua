1cc53248db998b4c1c80a8d5306c225a028a24af
speccius <50077694+speccius@users.noreply.github.com>
2019-05-05 20:00:04 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,23 @@
+local fortunes={
+"You will forget this",
+"A new perspective will come with the new year",
+"A hunch is creativity trying to tell you something",
+"Adventure can be real happiness",
+"All will go well with your new project",
+"All the troubles you have will pass away very quickly",
+"Don't just spend time; invest it",
+"Failure is the chance to do better next time",
+"Hate is never conquered by hate; it is conquered by love",
+"Fearles courage is the foundation of victory",
+"Think of what you want to do; then what you must",
+"Get your mind set; confidence will lead you on",
+"If you wish to see the best in others, show the best in thyself",
+"In this world of contradiction, it can be better to be merry than to be wise",
+"It is honourable to stand up for what is right, however unpopular it seems",
+"It is worth reviewing some old lessons",
+"Listen to everyone; ideas come from everywhere",
+"Living with a commitment to excellence shall take you far",
+"Miles are covered one step at a time",
+"New ideas could be profitable",
+}
+print(fortunes[math.random(1,#fortunes)])
